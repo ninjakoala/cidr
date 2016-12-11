57e09dfc337864c24b3168e74f4593eb5d743999
Neil Prosser <neil.prosser@gmail.com>
2016-12-11 14:15:09 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2"]]}}
+                   :plugins [[lein-midje "3.2.1"]]}}
 
   :eastwood {:namespaces [:source-paths]})

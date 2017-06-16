c6fda063a03bf5388da601eef1ea51d11c2959d9
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 20:26:12 +0100

    Remove lein-release

--- project.clj
+++ project.clj
@@ -5,8 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2.1"]
-                             [lein-release "1.0.5"]]}}
+                   :plugins [[lein-midje "3.2.1"]]}}
   :aliases {"test" ["midje"]}
-  :lein-release {:deploy-via :clojars}
   :eastwood {:namespaces [:source-paths]})

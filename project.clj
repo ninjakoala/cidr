2d97c65b17409922e569530790dac84ec45a6299
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 20:14:03 +0100

    Tweaks to project

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/ninjakoala/cidr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.8.0"]]
-
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2.1"]]}}
-
+                   :plugins [[lein-midje "3.2.1"]
+                             [lein-release "1.0.5"]]}}
+  :aliases {"test" ["midje"]}
+  :lein-release {:deploy-via :clojars}
   :eastwood {:namespaces [:source-paths]})

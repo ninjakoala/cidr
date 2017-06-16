2fb94f896241be0903f5a44cd7c808c619db931a
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 20:51:29 +0100

    Version 1.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ninjakoala/cidr "1.0.3"
+(defproject com.ninjakoala/cidr "1.0.4-SNAPSHOT"
   :description "Working with CIDR ranges"
   :url "https://github.com/ninjakoala/cidr"
   :license {:name "Eclipse Public License"

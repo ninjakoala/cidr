7af594e5f11612f077e91937f146c095443148e5
Neil Prosser <neil.prosser@gmail.com>
2017-12-01 20:35:02 +0000

    Update deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+  :profiles {:dev {:dependencies [[midje "1.9.0"]]
                    :plugins [[lein-midje "3.2.1"]]}}
   :aliases {"test" ["midje"]}
   :eastwood {:namespaces [:source-paths]}

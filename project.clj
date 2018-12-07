dd60b12a615ee59f4014a381d64eb88758dcef0a
Neil Prosser <neil.prosser@gmail.com>
2018-12-07 20:37:53 +0000

    Switch to clojure.test

--- project.clj
+++ project.clj
@@ -4,9 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[midje "1.9.2"]]
-                   :plugins [[lein-midje "3.2.1"]]}}
-  :aliases {"test" ["midje"]}
   :eastwood {:namespaces [:source-paths]}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
--- test/cidr/core_test.clj
+++ test/cidr/core_test.clj
@@ -1,36 +1,42 @@
 (ns cidr.core-test
   (:require [cidr.core :refer :all]
-            [midje.sweet :refer :all]))
+            [clojure.test :refer [deftest is testing]]))
 
-(fact "that an IP address which is in the range is true"
-      (in-range? "10.0.0.1" "10.0.0.1/32") => true)
+(deftest in-range?-test
+  (testing "that an IP address which is in the range is true"
+    (is (in-range? "10.0.0.1" "10.0.0.1/32")))
 
-(fact "that an IP address which isn't above the range is false"
-      (in-range? "9.255.255.255" "10.0.0.0/32") => false)
+  (testing "that an IP address which isn't above the range is false"
+    (is (not (in-range? "9.255.255.255" "10.0.0.0/32"))))
 
-(fact "that an IP address which isn't above the range is false"
-      (in-range? "10.0.0.1" "10.0.0.0/32") => false)
+  (testing "that an IP address which isn't above the range is false"
+    (is (not (in-range? "10.0.0.1" "10.0.0.0/32"))))
 
-(fact "that getting the IP range for a CIDR gives the min and max addresses"
-      (ip-range "10.0.0.0/0") => ["0.0.0.0" "255.255.255.255"])
+  (testing "that getting the IP range for a CIDR gives the min and max addresses"
+    (is (= ["0.0.0.0" "255.255.255.255"]
+           (ip-range "10.0.0.0/0")))))
 
-(fact "that getting the IP addresses in CIDR gives those addresses back in string form"
-      (ips-in-range "10.0.0.0/31") => ["10.0.0.0" "10.0.0.1"])
+(deftest ips-in-range-test
+  (testing "that getting the IP addresses in CIDR gives those addresses back in string form"
+    (is (= ["10.0.0.0" "10.0.0.1"]
+           (ips-in-range "10.0.0.0/31")))))
 
-(fact "that working out whether a range is completely contained within another works when they do overlap"
-      (completely-in-range? "10.0.0.1/32" "10.0.0.0/31") => true)
+(deftest completely-in-range?-test
+  (testing "that working out whether a range is completely contained within another works when they do overlap"
+    (is (completely-in-range? "10.0.0.1/32" "10.0.0.0/31")))
 
-(fact "that working out whether a range is completely contained within another works when they only partially overlap at the low-end"
-      (completely-in-range? "10.0.0.255/30" "10.0.0.255/32") => false)
+  (testing "that working out whether a range is completely contained within another works when they only partially overlap at the low-end"
+    (is (not (completely-in-range? "10.0.0.255/30" "10.0.0.255/32"))))
 
-(fact "that working out whether a range is completely contained within another works when they only partially overlap at the high-end"
-      (completely-in-range? "10.0.0.2/30" "10.0.0.3/32") => false)
+  (testing "that working out whether a range is completely contained within another works when they only partially overlap at the high-end"
+    (is (not (completely-in-range? "10.0.0.2/30" "10.0.0.3/32")))))
 
-(fact "that working out whether two ranges overlap returns false when they don't overlap at all"
-      (overlap? "9.0.0.0/8" "10.0.0.0/8") => false)
+(deftest overlap?-test
+  (testing "that working out whether two ranges overlap returns false when they don't overlap at all"
+    (is (not (overlap? "9.0.0.0/8" "10.0.0.0/8"))))
 
-(fact "that working out whether two ranges overlap returns false when they don't overlap at all"
-      (overlap? "10.0.0.255/30" "10.0.0.255/32") => true)
+  (testing "that working out whether two ranges overlap returns false when they don't overlap at all"
+    (is (overlap? "10.0.0.255/30" "10.0.0.255/32")))
 
-(fact "that working out whether two ranges overlap returns false when they don't overlap at all"
-      (overlap? "10.0.0.2/30" "10.0.0.3/32") => true)
+  (testing "that working out whether two ranges overlap returns false when they don't overlap at all"
+    (is (overlap? "10.0.0.2/30" "10.0.0.3/32"))))

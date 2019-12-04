59b465fc0a77b5831fac43189567f77fe1db9b02
Neil Prosser <neil.prosser@gmail.com>
2019-12-04 08:58:37 +0000

    Update Clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ninjakoala/cidr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :eastwood {:namespaces [:source-paths]}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
